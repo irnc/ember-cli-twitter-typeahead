f60400ba21ec2c0067a5003e13fd9c84b4a7aecd
pzubkou <pzubkou@thumbtack.net>
2015-06-16 14:28:19 +0300

    Convert addon to contemporary convention over addon file layout

---
+++ app/components/twitter-typeahead.js
@@ -0,0 +1,4 @@
+import Ember from 'ember';
+import TwitterTypeahead from 'ember-cli-twitter-typeahead/components/twitter-typeahead';
+
+export default TwitterTypeahead;
--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 'use strict';
 
-var path = require("path");
-var fs = require("fs");
-
-function unwatchedTree(dir) {
-    return {
-      read:    function() { return dir; },
-      cleanup: function() { }
-    };
-}
-
 module.exports = {
   name: 'Ember CLI Twitter Typeahead',
 
@@ -17,13 +7,5 @@ module.exports = {
     this._super.included(app);
 
     app.import(app.bowerDirectory + '/typeahead.js/dist/typeahead.bundle.js');
-  },
-
-  treeFor: function treeFor(name) {
-    var treePath = path.join('node_modules', 'ember-cli-twitter-typeahead', name + '-addon');
-
-    if (fs.existsSync(treePath)) {
-      return unwatchedTree(treePath);
-    }
   }
 };
